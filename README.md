498ed4cd13f6c917e51a3c7f7419d7a03120caec
mixassio <mixail.bondarev@gmail.com>
2017-03-20 21:25:02 +0300

    Add select path from console and change README.md

--- README.md
+++ README.md
@@ -5,6 +5,18 @@ And displays in the console ten most popular words in this file in descending or
 Works with any languages.
 To work the module, you must specify the path to the text file in the argument of the function load_data (filepath).
 
+# HOW RUN
+
+Example of script launch on Linux, Python 3.5:
+
+```#!bash
+
+$ python lang_frequency.py <path to file>
+
+$ [('и', 10132), ('в', 4863), ('не', 4220), ('что', 3646), ('на', 3200), ('с', 3023), ('он', 3016), ('как', 1913), ('его', 1876), ('к', 1777)]
+    ```
+
+
 # Project Goals
 
 The code is written for educational purposes. Training course for web-developers - [DEVMAN.org](https://devman.org)
--- lang_frequency.py
+++ lang_frequency.py
@@ -3,27 +3,26 @@ import os
 import chardet
 import re
 import collections
+import sys
 
 
 def load_data(filepath):
     if os.path.exists(filepath):
-        char_type = chardet.detect(open(filepath, "rb").read())['encoding']
+        with open(filepath, 'rb') as file_text:
+            char_type = chardet.detect(file_text.read())['encoding']
         with codecs.open(filepath, 'rb', encoding=char_type) as file_text:
             return file_text.read()
 
 
 def get_most_frequent_words(text):
-    text_file = re.sub(r'[\.,\,,\?,\!,\-,\),\(,\–,\«,\»,\…,\[,\],\:,\;,\d]', '', text)
-    list_words = text_file.split()
-    word_count = collections.Counter()
-    for word in list_words:
-        word_count[word] += 1
-    print(word_count.most_common()[:10])
+    return collections.Counter(re.sub(r'[^\w\s]', '', text).split()).most_common(10)
 
 
 
 if __name__ == '__main__':
-    text_file = load_data('book1.txt')
-    print(text_file)
-    get_most_frequent_words(text_file)
+    if sys.argv[1]:
+        text_file = load_data(sys.argv[1])
+    else:
+        print('input path to file')
+    print(get_most_frequent_words(text_file))
 
