ecb48c7176e5102e5118e7412246bdbd0c7ff3de
mixassio <mixail.bondarev@gmail.com>
2017-03-21 13:42:04 +0000

    change try-exept on argparse

--- lang_frequency.py
+++ lang_frequency.py
@@ -4,7 +4,13 @@ import chardet
 import re
 import collections
 import sys
+import argparse
 
+def create_parser ():
+    parser = argparse.ArgumentParser()
+    parser.add_argument ('filepath')
+    parser.add_argument ('-c', '--cost_result', nargs='?') 
+    return parser
 
 def load_data(filepath):
     if os.path.exists(filepath):
@@ -13,22 +19,20 @@ def load_data(filepath):
         with codecs.open(filepath, 'rb', encoding=char_type) as file_text:
             return file_text.read()
 
-
 def get_most_frequent_words(text, cost_result=10):
     return collections.Counter(re.sub(r'[^\w\s]', '', text).split()).most_common(cost_result)
 
 
 
 if __name__ == '__main__':
-    try:
-        if sys.argv[1]:
-            text_file = load_data(sys.argv[1])
-    except IndexError:
-        print('input path to file')
-    try:
-        if sys.argv[2] and isinstance(int(sys.argv[2]), int):
-            print(get_most_frequent_words(text_file, int(sys.argv[2])))
-    except (IndexError, ValueError):
+    parser = create_parser()
+    namespace = parser.parse_args()
+    text_file = load_data(namespace.filepath)
+    print(namespace)
+    if namespace.cost_result:
+        print(get_most_frequent_words(text_file, int(namespace.cost_result)))
+    else:
         print(get_most_frequent_words(text_file))
     
+   
  
